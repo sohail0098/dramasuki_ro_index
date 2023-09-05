530d4e4cbe8cc3811e0b5b9406148e2836f8b123
sohail0098 <mohammedsohailahmed18@gmail.com>
2023-09-05 10:57:07 -0400

    Logo URL fix

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 const headerLogoConatiner = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html'
+  location.href = '#'
 })
 
 document.getElementById("defaultOpen").click();
